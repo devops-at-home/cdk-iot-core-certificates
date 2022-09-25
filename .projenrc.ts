eac2d9c79eeac38e6e6452fe2a8fb111ad795bdf
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2022-09-25 22:30:42 +0800

    Update deps (#71)
    * add vscode settings
    
    * update formatting
    
    * update deps

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,33 +1,33 @@
 import { awscdk } from 'projen';
 
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'DevOps@Home',
-  authorAddress: 'devops.at.home@gmail.com',
-  cdkVersion: '2.33.0',
-  defaultReleaseBranch: 'main',
-  eslint: false,
-  name: 'cdk-iot-core-certificates',
-  projenrcTs: true,
-  repositoryUrl: 'git@github.com:devops-at-home/cdk-iot-core-certificates.git',
-  license: 'MIT',
-  gitignore: ['.idea', '.DS_Store'],
-  devDeps: ['esbuild', '@types/aws-lambda', 'aws-lambda', 'aws-sdk', 'aws-cloudformation-custom-resource'],
-  prettier: true,
-  prettierOptions: {
-    settings: {
-      printWidth: 120,
-      tabWidth: 2,
-      singleQuote: true,
+    author: 'DevOps@Home',
+    authorAddress: 'devops.at.home@gmail.com',
+    cdkVersion: '2.43.1',
+    defaultReleaseBranch: 'main',
+    eslint: false,
+    name: 'cdk-iot-core-certificates',
+    projenrcTs: true,
+    repositoryUrl: 'git@github.com:devops-at-home/cdk-iot-core-certificates.git',
+    license: 'MIT',
+    gitignore: ['.idea', '.DS_Store'],
+    devDeps: ['esbuild', '@types/aws-lambda', 'aws-lambda', 'aws-sdk', 'aws-cloudformation-custom-resource'],
+    prettier: true,
+    prettierOptions: {
+        settings: {
+            printWidth: 120,
+            tabWidth: 4,
+            singleQuote: true,
+        },
     },
-  },
-  githubOptions: {
-    pullRequestLint: false,
-  },
-  autoApproveUpgrades: true,
-  autoApproveOptions: {
-    allowedUsernames: ['devops-at-home'],
-  },
-  majorVersion: 1,
+    githubOptions: {
+        pullRequestLint: false,
+    },
+    autoApproveUpgrades: true,
+    autoApproveOptions: {
+        allowedUsernames: ['devops-at-home'],
+    },
+    majorVersion: 1,
 });
 
 project.jest!.addTestMatch('**/?(*.)@(spec|test).[tj]s?(x)');
